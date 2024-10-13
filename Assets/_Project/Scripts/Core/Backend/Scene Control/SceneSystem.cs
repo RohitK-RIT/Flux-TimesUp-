e54f40df7e443e73caeb7a220cd86a596c80bc5d
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-10-13 18:10:19 -0400

    Added loading scene logic

--- Assets/_Project/Scripts/Core/Backend/Scene Control/SceneSystem.cs
+++ Assets/_Project/Scripts/Core/Backend/Scene Control/SceneSystem.cs
@@ -6,21 +6,33 @@ namespace _Project.Scripts.Core.Backend.Scene_Control
 {
     public class SceneSystem : BaseSystem<SceneSystem>
     {
+        [SerializeField] private string loadingSceneName;
+
         private Coroutine _loadSceneCoroutine;
 
         public void LoadScene(SceneLoadRequest request)
         {
-            if(_loadSceneCoroutine != null)
+            if (_loadSceneCoroutine != null)
                 StopCoroutine(_loadSceneCoroutine);
-            
+
             _loadSceneCoroutine = StartCoroutine(LoadSceneCoroutine(request));
         }
-        
-        private static IEnumerator LoadSceneCoroutine(SceneLoadRequest request)
+
+        private IEnumerator LoadSceneCoroutine(SceneLoadRequest request)
         {
-            var asyncOperation = SceneManager.LoadSceneAsync(request.SceneName, request.Mode);
-            
-            yield return asyncOperation;
+            var sceneOp = SceneManager.LoadSceneAsync(loadingSceneName, LoadSceneMode.Additive);
+            yield return sceneOp;
+
+
+            sceneOp = SceneManager.LoadSceneAsync(request.SceneName, request.Mode);
+
+            if (sceneOp == null)
+            {
+                Debug.LogError($"Failed to load scene {request.SceneName}");
+                yield break;
+            }
+
+            yield return sceneOp;
         }
     }
 }
