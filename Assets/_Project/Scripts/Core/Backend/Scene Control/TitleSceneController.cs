874397aa9d091028a919eefd4e12943c82dd57d9
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-10-13 22:00:06 -0400

    Added comments

--- Assets/_Project/Scripts/Core/Backend/Scene Control/TitleSceneController.cs
+++ Assets/_Project/Scripts/Core/Backend/Scene Control/TitleSceneController.cs
@@ -3,15 +3,25 @@ using UnityEngine.SceneManagement;
 
 namespace _Project.Scripts.Core.Backend.Scene_Control
 {
+    /// <summary>
+    /// Controller for the title scene.
+    /// </summary>
     public class TitleSceneController : MonoBehaviour
     {
+        /// <summary>
+        /// Name of the main menu scene.
+        /// </summary>
         [SerializeField] private string mainMenuSceneName;
 
         private void Start()
         {
+            // Load the main menu scene after 2.5 seconds.
             Invoke(nameof(LoadMainMenuScene), 2.5f);
         }
 
+        /// <summary>
+        /// Load the main menu scene.
+        /// </summary>
         private void LoadMainMenuScene()
         {
             SceneSystem.Instance.LoadScene(new SceneLoadRequest(mainMenuSceneName, LoadSceneMode.Single));
