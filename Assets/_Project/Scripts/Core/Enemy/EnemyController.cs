18a6fb3dc162e550d5c4e3541212f71bf7c43d9a
Roosa Mozoomdar <rm7132@g.rit.edu>
2024-10-19 07:46:55 -0400

    Added player tag to player prefab and added comments

--- Assets/_Project/Scripts/Core/Enemy/EnemyController.cs
+++ Assets/_Project/Scripts/Core/Enemy/EnemyController.cs
@@ -10,29 +10,33 @@ public class EnemyController : PlayerController
     protected override void Awake()
     {
         base.Awake();
-
         _enemyInputController = GetComponent<EnemyInputController>();
     }
     
     private void OnEnable()
     {
+        // Subscribe to attack input events on enable
         _enemyInputController.OnAttackInputBegan += BeginAttack;
         _enemyInputController.OnAttackInputEnded += EndAttack;
         
+        // Subscribe to look input updates on enable
         _enemyInputController.OnLookInputUpdated += LookInputUpdated;
     }
 
     private void OnDisable()
     {
+        // Subscribe to attack input events on disable
         _enemyInputController.Disable();
         _enemyInputController.OnAttackInputBegan -= BeginAttack;
         _enemyInputController.OnAttackInputEnded -= EndAttack;
         
+        // Subscribe to look input updates on disable
         _enemyInputController.OnLookInputUpdated -= LookInputUpdated;
     }
 
     protected override void LookInputUpdated(Vector2 lookInput)
     {
+        // Update the look direction in the MovementController based on the new input.
         MovementController.lookDirection = lookInput;
     }
 }
