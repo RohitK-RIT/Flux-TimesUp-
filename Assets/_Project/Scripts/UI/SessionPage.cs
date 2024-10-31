8dd20dfb3975088752820e97f7f71b8bdfe8cfcf
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-10-31 01:31:23 -0400

    Created and join lobby functionality implemented

---
+++ Assets/_Project/Scripts/UI/SessionPage.cs
@@ -0,0 +1,36 @@
+﻿using _Project.Scripts.Core.Backend;
+using TMPro;
+using UnityEngine;
+
+namespace _Project.Scripts.UI
+{
+    public class SessionPage : UIPage
+    {
+        [SerializeField] private TMP_Text lobbyCodeText;
+        [SerializeField] private TMP_InputField lobbyCodeInputField;
+        [SerializeField] private TMP_Text statusText;
+
+        public async void OnCreateButtonClicked()
+        {
+            // Start the game in host mode
+            var partyCode = await NetworkSystem.Instance.CreateSession();
+            lobbyCodeText.text = partyCode;
+        }
+
+        public async void OnJoinButtonClicked()
+        {
+            // Start the game in client mode
+            var status = await NetworkSystem.Instance.JoinSession(lobbyCodeInputField.text);
+            if (status.Item1)
+            {
+                statusText.text = status.Item2;
+                statusText.color = Color.green;
+            }
+            else
+            {
+                statusText.text = status.Item2;
+                statusText.color = Color.red;
+            }
+        }
+    }
+}
