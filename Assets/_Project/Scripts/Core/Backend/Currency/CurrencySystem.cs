03af7d0325afdec0b5deebac6d5df7ab13d8eca0
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-11-16 05:23:32 -0500

    Added comments

--- Assets/_Project/Scripts/Core/Backend/Currency/CurrencySystem.cs
+++ Assets/_Project/Scripts/Core/Backend/Currency/CurrencySystem.cs
@@ -3,23 +3,42 @@ using UnityEngine;
 
 namespace _Project.Scripts.Core.Backend.Currency
 {
+    /// <summary>
+    /// System to handle currency.
+    /// </summary>
     public class CurrencySystem : BaseSystem<CurrencySystem>
     {
         protected override bool IsPersistent => true;
 
+        /// <summary>
+        /// The starting amount of coins for a new wallet.
+        /// </summary>
         private const int StartingCoins = 2000;
 
+        /// <summary>
+        /// Dictionary of wallets.
+        /// </summary>
         private readonly Dictionary<string, Wallet> _wallets = new();
 
+        /// <summary>
+        /// Creates a new wallet and returns the wallet ID.
+        /// </summary>
+        /// <returns></returns>
         public string CreateWallet()
         {
+            // Create a new wallet and add it to the dictionary
             var wallet = new Wallet(StartingCoins);
             var walletID = wallet.GetHashCode().ToString();
             _wallets.Add(walletID, wallet);
 
+            // Return the wallet ID
             return walletID;
         }
 
+        /// <summary>
+        /// Removes a wallet from the system.
+        /// </summary>
+        /// <param name="walletID">wallet ID to remove</param>
         public void RemoveWallet(string walletID)
         {
             // Remove and destroy the wallet
@@ -27,19 +46,37 @@ namespace _Project.Scripts.Core.Backend.Currency
             _wallets.Remove(walletID);
         }
 
+        /// <summary>
+        /// Adds coins to a wallet.
+        /// </summary>
+        /// <param name="walletID">wallet ID to add coins</param>
+        /// <param name="amount">amount of coins to add</param>
         public void AddCoins(string walletID, int amount)
         {
+            // Get the wallet and add coins
             var wallet = GetWallet(walletID);
             wallet?.AddCoins(amount);
         }
 
+        /// <summary>
+        /// Removes coins from a wallet.
+        /// </summary>
+        /// <param name="walletID">wallet ID to remove coins</param>
+        /// <param name="amount">amount of coins to remove</param>
+        /// <returns></returns>
         public bool RemoveCoins(string walletID, int amount)
         {
+            // Get the wallet and remove coins
             var wallet = GetWallet(walletID);
             return wallet != null && wallet.RemoveCoins(amount);
         }
 
 
+        /// <summary>
+        /// Get a wallet by its ID.
+        /// </summary>
+        /// <param name="walletID">wallet ID to return the wallet</param>
+        /// <returns>wallet of the specified ID</returns>
         private Wallet GetWallet(string walletID)
         {
             var wallet = _wallets.GetValueOrDefault(walletID);
@@ -50,8 +87,14 @@ namespace _Project.Scripts.Core.Backend.Currency
             return null;
         }
 
+        /// <summary>
+        /// Get the amount of coins in a wallet.
+        /// </summary>
+        /// <param name="walletID">wallet ID to return the coins in it</param>
+        /// <returns>amount of coins in a wallet of specified ID</returns>
         public int GetCoins(string walletID)
         {
+            // Get the wallet and return the coins value
             var wallet = GetWallet(walletID);
             return wallet?.Coins ?? 0;
         }
--- Assets/_Project/Scripts/Core/Backend/Currency/Wallet.cs
+++ Assets/_Project/Scripts/Core/Backend/Currency/Wallet.cs
@@ -1,23 +1,44 @@
 ﻿namespace _Project.Scripts.Core.Backend.Currency
 {
+    /// <summary>
+    /// Class to store the player's wallet and handle adding and removing coins.
+    /// </summary>
     internal class Wallet
     {
-        public int Coins { get; private set; }
+        /// <summary>
+        /// The amount of coins in the wallet.
+        /// </summary>
+        internal int Coins { get; private set; }
 
-        public Wallet(int coins)
+        /// <summary>
+        /// Constructor for the wallet.
+        /// </summary>
+        /// <param name="coins">starting amount in the wallet</param>
+        internal Wallet(int coins)
         {
             Coins = coins;
         }
-        
+
+        /// <summary>
+        /// Adds coins to the wallet.
+        /// </summary>
+        /// <param name="amount">amount to add</param>
         internal void AddCoins(int amount)
         {
             Coins += amount;
         }
-        
-        public bool RemoveCoins(int amount)
+
+        /// <summary>
+        /// Removes coins from the wallet.
+        /// </summary>
+        /// <param name="amount">amount to remove</param>
+        /// <returns>was successfully removed</returns>
+        internal bool RemoveCoins(int amount)
         {
+            // Check if there are enough coins in the wallet
             if (Coins < amount) return false;
-            
+
+            // Remove the coins
             Coins -= amount;
             return true;
         }
